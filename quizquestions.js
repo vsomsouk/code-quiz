8bbea3f703f5b286be9cce558a38c3763a25f073
Vanida Somchaleunsouk <vsomsouk@gmail.com>
2020-04-25 19:31:30 -0500

    Created new JS file and renamed questions file to Quizquestions.

---
+++ quizquestions.js
@@ -0,0 +1,48 @@
+// Building questions
+
+let questions = [
+    {
+        question: "What does HTML stand for?",
+        firstanswer: "Hello Tomorrow Mid-Day Language",
+        secondanswer: "Hypertext Mixed Language",
+        thirdanswer: "No meaning - HTML",
+        fourthanswer: "Hypertext Markup Language",
+        correct: "fourthanswer"
+
+    },{
+        question: "What does CSS stand for?",
+        firstanswer: "Falling Techniques of Virtual Layouts",
+        secondanswer: "Cascading Stepping Stones",
+        thirdanswer: "Cascading Style Sheets",
+        fourthanswer: "Control Style Sheets",
+        correct: "thirdanswer"
+    },{
+        question: "What is Boot Strap?",
+        firstanswer: "Open-Source CSS Framework",
+        secondanswer: "Straps with Boots",
+        thirdanswer: "Boots You Wear to the Rodeo",
+        fourthanswer: "Rebooting Code",
+        correct: "firstanswer"
+    },{
+        question: "What is JavaScript?",
+        firstanswer: "Scripting language to create content with multimedia and animate images",
+        secondanswer: "A coffeeshop where you can create your TV and play scripts",
+        thirdanswer: "A type of caligraphy",
+        fourthanswer: "Car brand",
+        correct: "firstanswer"
+    },{
+        question: "What is JQuery?",
+        firstanswer: "HTML Library",
+        secondanswer: "JavaScript Library",
+        thirdanswer: "CSS Library",
+        fourthanswer: "JQ Library",
+        correct: "secondanswer"
+    },{
+        question: "BONUS! Apple or Windows? *Don't come after me*",
+        firstanswer: "Apple",
+        secondanswer: "Windows",
+        thirdanswer: "Both are great",
+        fourthanswer: "Nope."
+        correct: "thirdanswer"
+    }
+];
